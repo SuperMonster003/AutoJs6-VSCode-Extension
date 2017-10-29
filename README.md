94c85afd31529890557048839ba4186d2454756e
hyb1996 <946994919@qq.com>
2017-10-29 22:15:45 +0800

    initial commit

---
+++ README.md
@@ -0,0 +1,42 @@
+# Auto.js-VSCodeExt README
+
+This is a simple development plugin for Android app [Auto.js](https://github.com/hyb1996/NoRootScriptDroid).
+
+## Features
+
+* Show log from Auto.js on Console.(Press `Ctrl+Shift+Y` to open console)
+* Run and stop script on attached device(s)
+
+##  Usage
+
+Press `Ctrl+Shift+P` and enter `Auto.js` you can see several commands:
+
+（Unfinished...)
+
+## Known Issues
+
+Calling out known issues can help limit users opening duplicate issues against your extension.
+
+## Release Notes
+
+Users appreciate release notes as you update your extension.
+
+### 1.0.0
+
+Initial release of ...
+
+### 1.0.1
+
+Fixed issue #.
+
+### 1.1.0
+
+Added features X, Y, and Z.
+
+-----------------------------------------------------------------------------------------------------------
+
+### For more information
+
+* [Github repo](https://github.com/hyb1996/Auto.js-VSCode-Extension)
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,111 @@
+'use strict';
+import * as vscode from 'vscode';
+import {AutoJs, Device} from './autojs';
+
+
+var server = new AutoJs(1209);
+var recentDevice = null;
+
+server
+    .on('connect', ()=>{
+        vscode.window.showInformationMessage('Auto.js server running');    
+    })
+    .on('new_device', (device: Device)=>{
+        var messageShown = false;
+        var showMessage = () =>{
+            if(messageShown)
+                return;
+            vscode.window.showInformationMessage('New device attached: ' + device);  
+            messageShown = true;
+        };
+        setTimeout(showMessage, 1000);
+        device.on('data:device_name', showMessage);
+    });
+
+class Extension {
+    
+    startServer(){
+        server.listen();
+    }
+    
+    stopServer(){
+        server.disconnect();
+        vscode.window.showInformationMessage('Auto.js server stopped');
+    }
+    
+    run(){
+        this.runOn(server);
+    }
+
+    stop(){
+        server.send({
+            'type': 'command',
+            'view_id': vscode.window.activeTextEditor.document.fileName,
+            'command': 'stop',
+        })
+    }
+
+    stopAll(){
+        server.send({
+            'type': 'command',
+            'command': 'stopAll'
+        })
+    }
+
+    rerun(){
+        let editor = vscode.window.activeTextEditor;
+        server.send({
+            'type': 'command',
+            'command': 'rerun',
+            'view_id': editor.document.fileName,
+            'name': editor.document.fileName,
+            'script': editor.document.getText()
+        });
+    }
+    
+    runOnDevice(){
+        let devices = server.devices;
+        if(recentDevice){
+            let i = devices.indexOf(recentDevice);
+            if(i > 0){
+                devices = devices.slice(0);
+                devices[i] = devices[0];
+                devices[0] = recentDevice;
+            }
+        }
+        let names = devices.map(device => device.toString());
+        vscode.window.showQuickPick(names)
+             .then(select => {
+                let device = devices[names.indexOf(select)];
+                recentDevice = device;
+                this.runOn(device);
+             });
+    }
+    
+    runOn(target: AutoJs | Device){
+        let editor = vscode.window.activeTextEditor;
+        target.send({
+            'type': 'command',
+            'command': 'run',
+            'view_id': editor.document.fileName,
+            'name': editor.document.fileName,
+            'script': editor.document.getText()
+        })
+    }
+};
+
+
+const commands = ['startServer', 'stopServer', 'run', 'runOnDevice', 'stop', 'stopAll', 'rerun'];
+let extension = new Extension();
+
+export function activate(context: vscode.ExtensionContext) {
+    console.log('extension "auto-js-vscodeext" is now active.');
+    commands.forEach((command)=>{
+        let action: Function = extension[command];
+        context.subscriptions.push(vscode.commands.registerCommand('extension.' + command, action.bind(extension)));
+    })
+}
+
+export function deactivate() {
+    server.disconnect();
+}
