f0d5acadb98daf492d4e2d2410f823aa74f3e846
hyb1996 <946994919@qq.com>
2018-10-18 17:55:21 +0800

    save works:

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,7 +2,3 @@
 All notable changes to the "auto-js-vscodeext" extension will be documented in this file.
 
 Check [Keep a Changelog](http://keepachangelog.com/) for recommendations on how to structure this file.
-
-## 仅支持Auto.js版本4.1以上
-
-本版本插件仅支持Auto.js版本4.1以上。
--- src/extension.ts
+++ src/extension.ts
@@ -1,9 +1,11 @@
 'use strict';
 import * as vscode from 'vscode';
 import { AutoJsDebugServer, Device } from './autojs-debug';
-
+import * as oldAutojs from './autojs-debug-old';
 
 var server = new AutoJsDebugServer(9317);
+var oldServer = new oldAutojs.AutoJsDebugServer(1209);
+
 var recentDevice = null;
 
 server
@@ -22,35 +24,72 @@ server
         device.on('data:device_name', showMessage);
     });
 
+oldServer
+    .on('connect', () => {
+        console.log('Auto.js server running');
+    })
+    .on('new_device', (device: Device) => {
+        var messageShown = false;
+        var showMessage = () => {
+            if (messageShown)
+                return;
+            vscode.window.showInformationMessage('New device attached: ' + device);
+            messageShown = true;
+        };
+        setTimeout(showMessage, 1000);
+        device.on('data:device_name', showMessage);
+    });
+
+
+
 class Extension {
 
     startServer() {
         server.listen();
+        oldServer.listen();
     }
 
     stopServer() {
         server.disconnect();
+        oldServer.disconnect();
         vscode.window.showInformationMessage('Auto.js server stopped');
     }
 
     run() {
         this.runOn(server);
+        this.runOn(oldServer);
     }
 
     stop() {
         server.sendCommand('stop', {
             'id': vscode.window.activeTextEditor.document.fileName,
+        });
+        oldServer.send({
+            'type': 'command',
+            'view_id': vscode.window.activeTextEditor.document.fileName,
+            'command': 'stop',
         })
     }
 
     stopAll() {
-        server.sendCommand('stopAll')
+        server.sendCommand('stopAll');
+        oldServer.send({
+            'type': 'command',
+            'command': 'stopAll'
+        })
     }
 
     rerun() {
         let editor = vscode.window.activeTextEditor;
         server.sendCommand('rerun', {
             'id': editor.document.fileName,
+            'name': editor.document.fileName,
+            'script': editor.document.getText()
+        });
+        oldServer.send({
+            'type': 'command',
+            'command': 'rerun',
+            'view_id': editor.document.fileName,
             'name': editor.document.fileName,
             'script': editor.document.getText()
         });
@@ -61,7 +100,8 @@ class Extension {
     }
 
     selectDevice(callback) {
-        let devices = server.devices;
+        let devices: Array<Device | oldAutojs.Device> = server.devices;
+        devices = devices.concat(oldServer.devices);
         if (recentDevice) {
             let i = devices.indexOf(recentDevice);
             if (i > 0) {
@@ -79,13 +119,24 @@ class Extension {
             });
     }
 
-    runOn(target: AutoJsDebugServer | Device) {
+    runOn(target: AutoJsDebugServer | Device | oldAutojs.Device | oldAutojs.AutoJsDebugServer) {
         let editor = vscode.window.activeTextEditor;
-        target.sendCommand('run', {
-            'id': editor.document.fileName,
-            'name': editor.document.fileName,
-            'script': editor.document.getText()
-        })
+        if (target instanceof oldAutojs.Device || target instanceof oldAutojs.AutoJsDebugServer) {
+            target.send({
+                'type': 'command',
+                'command': 'run',
+                'view_id': editor.document.fileName,
+                'name': editor.document.fileName,
+                'script': editor.document.getText()
+            })
+        } else {
+            target.sendCommand('run', {
+                'id': editor.document.fileName,
+                'name': editor.document.fileName,
+                'script': editor.document.getText()
+            })
+        }
+
     }
 
     save() {
@@ -96,13 +147,23 @@ class Extension {
         this.selectDevice(device => this.saveTo(device));
     }
 
-    saveTo(target: AutoJsDebugServer | Device) {
+    saveTo(target: AutoJsDebugServer | Device | oldAutojs.Device) {
         let editor = vscode.window.activeTextEditor;
-        target.sendCommand('save', {
-            'id': editor.document.fileName,
-            'name': editor.document.fileName,
-            'script': editor.document.getText()
-        })
+        if (target instanceof oldAutojs.Device || target instanceof oldAutojs.AutoJsDebugServer) {
+            target.send({
+                'command': 'save',
+                'type': 'command',
+                'view_id':  editor.document.fileName,
+                'name':  editor.document.fileName,
+                'script': editor.document.getText()
+            })
+        } else {
+            target.sendCommand('save', {
+                'id': editor.document.fileName,
+                'name': editor.document.fileName,
+                'script': editor.document.getText()
+            })
+        }
     }
 };
 
